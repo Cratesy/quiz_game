ab10a8b91f81e6af969fe569498118bb3d3ffa20
Michael Crates <michaelcrates89@hotmail.co.uk>
2021-03-21 10:09:31 +0000

    added construction of game container + questions array

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,7 +1,54 @@
-const start = document.querySelector(".start-button");
+const startButtonElement = document.getElementById("start-button");
+const startGameDiv = document.getElementById("start-container");
+const bodyElement = document.body;
 
-function startQuiz(event) {
-  console.log(event.target);
-}
+const questions = [
+  {
+    question: "What does Thor want "'another'" of when he's in the diner??",
+    answers:[
+      {text: "A cup of coffee", correct: true},
+      {text: "A slice of pie", correct: false},
+      {text: "A piece of toast", correct: false},
+      {text: "A stack of pancakes", correct: false},
+    ]
+  },
+];
+const constructGameContainer = () => {
+  const gameContainerDiv = document.createElement("div");
+  gameContainerDiv.setAttribute("class", "game-container");
 
-start.addEventListener("click", startQuiz);
+  const questionContainer = document.createElement("div");
+  questionContainer.setAttribute("class", "question-container");
+
+  const answerContainer = document.createElement("div");
+  answerContainer.setAttribute("class", "answer-container");
+
+  const button1 = document.createElement("button");
+  button1.setAttribute("id", "button1");
+  const button2 = document.createElement("button");
+  button1.setAttribute("id", "button2");
+  const button3 = document.createElement("button");
+  button1.setAttribute("id", "button3");
+  const button4 = document.createElement("button");
+  button1.setAttribute("id", "button4");
+
+  return gameContainerDiv;
+};
+
+const startQuiz = () => {
+  // replace this with game container div
+
+  // construct the game div in js
+  const gameDivElement = constructGameContainer();
+
+  console.log(gameDivElement);
+
+  // remove start-game
+  bodyElement.removeChild(startGameDiv);
+
+  bodyElement.appendChild(gameDivElement);
+
+  // insert the game-container
+};
+
+startButtonElement.addEventListener("click", startQuiz);
