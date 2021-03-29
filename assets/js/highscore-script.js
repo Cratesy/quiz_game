713f44dcd89c9b0a1aab772ff8dc728b63d649f2
Michael Crates <michaelcrates89@hotmail.co.uk>
2021-03-29 21:09:58 +0100

    update to renderHighScore

--- assets/js/highscore-script.js
+++ assets/js/highscore-script.js
@@ -1,5 +1,6 @@
 const back = document.querySelector(".back-button");
 const clear = document.querySelector(".clear-high-score");
+const tableBody = document.getElementById("tbody");
 
 // go back
 const backToMainScreen = () => {
@@ -22,7 +23,20 @@ const getHighScoreFromLocal = () => {
 };
 
 // constructing highScoreTable
-const renderHighScoreTable = (highScores) => {};
+const renderHighScoreTable = () => {
+  const tr = tableBody.createElement("tr");
+  // const td = document.createElement("td");
+  // td.textContent = rank;
+  const tdInput = tr.createElement("td");
+  tdInput.textContent = userInputValue;
+  const tdScore = tr.createElement("td");
+  tdScore.textContent = scoreTime;
+  // td.append(td);
+  tr.append(tr);
+  tdInput.append(tdInput);
+  tdScore.append(tdScore);
+  return tr;
+};
 
 // loading highScores onload
 const onLoad = () => {
